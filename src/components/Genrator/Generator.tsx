0ae74b4b602940a3538059063acd4ca37c818d1a
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-11 10:12:34 +0200

    feat: use state

--- src/components/Genrator/Generator.tsx
+++ src/components/Genrator/Generator.tsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+
 import { Text } from '@ui/atoms/Text';
 import { Button } from '@ui/atoms/Button';
 
 export const Generator = () => {
-  const id = uuidv4();
+  const state = useState(uuidv4());
+
+  // let id = uuidv4();
+
+  const handleClick = () => {
+    // id = uuidv4();
+    state[1](uuidv4());
+
+    console.log();
+  }
+
   return (
     <>
-      <Text>{id}</Text>
-      <button>Regenerate</button>
+      <Text>{state[0]}</Text>
+      <button onClick={handleClick}>Regenerate</button>
       {/* <Button /> */}
     </>
   );
