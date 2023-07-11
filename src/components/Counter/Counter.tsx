91f3668ce002c1d1c73602b259bacd6779e7477d
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-11 15:56:15 +0200

    feat: counter component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+
 export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount((value) => value + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, []);
+
   return (
     <div>
       Count: {count}
