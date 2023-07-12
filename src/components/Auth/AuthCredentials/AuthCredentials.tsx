7cdc0f5edbcae25a8ecae46afbf51176509d9aba
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-12 09:59:50 +0200

    contexts

--- src/components/Auth/AuthCredentials/AuthCredentials.tsx
+++ src/components/Auth/AuthCredentials/AuthCredentials.tsx
@@ -1,11 +1,19 @@
-import { useContext } from 'react';
+import { MouseEventHandler, useContext } from 'react';
 import { AuthContext } from '@pages/HomePage';
+import { Button } from '@ui/atoms';
 
 
 export const AuthCredentials = () => {
   const context = useContext(AuthContext);
 
+  const handleClick: MouseEventHandler<HTMLButtonElement> = () => {
+    context.setIsLogged(value => !value);
+  }
+
   return (
-    <div>Is user logged? {context.isLogged ? 'Yes' : 'No'} </div>
+    <>
+      <div>Is user logged? {context.isLogged ? 'Yes' : 'No'} </div>
+      <Button label="Change value" onClick={handleClick} />
+    </>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,30 @@
 import { AuthInfo } from "@components/Auth/AuthInfo";
 import { Generator } from "@components/Genrator";
 import { Text, Button } from "@ui/atoms";
-import { createContext } from "react";
+import { Dispatch, SetStateAction, createContext, useState } from "react";
 
 interface Auth {
   isLogged: boolean;
+  // setIsLogged: (value: boolean) => void
+  setIsLogged: Dispatch<SetStateAction<boolean>>
 }
 
 const defaultValues: Auth = {
   isLogged: false,
+  setIsLogged: () => null,
 }
 
 export const AuthContext = createContext<Auth>(defaultValues);
 
 export const HomePage = () => {
+  const [isLogged, setIsLogged] = useState(false);
+
   return (
     <>
-      <AuthContext.Provider value={defaultValues}>
+      <AuthContext.Provider value={{
+        isLogged,
+        setIsLogged
+      }}>
         <AuthInfo />
       </AuthContext.Provider>
 
