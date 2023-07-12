00f2f2847d12c287e20ee604ba5827acf56d81c3
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-12 16:55:16 +0200

    feat: component with reducers

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,13 +1,46 @@
 import { Button } from '@ui/atoms';
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+interface State {
+  count: number;
+}
+
+enum ActionType {
+  INCREASE = 'increase',
+  DECREASE = 'decrease',
+}
+
+interface Action {
+  type: ActionType,
+  payload?: unknown
+}
+
+const initialValues: State = {
+  count : 0
+}
+
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case ActionType.DECREASE:
+      return { count: state.count - 1 };
+    case ActionType.INCREASE:
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+const decrease = () => ({ type: ActionType.DECREASE });
+const increase = () => ({ type: ActionType.INCREASE });
 
 export const Stepper = () => {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialValues);
   return (
     <div>
-      <Button label="-" onClick={() => setCount(value => value - 1)} />
-      {count}
-      <Button label="+" onClick={() => setCount(value => value + 1)} />
+      <Button label="-" onClick={() => dispatch(decrease())} />
+      {state.count}
+      <Button label="+" onClick={() => dispatch({ type: ActionType.INCREASE })} />
     </div>
   );
 }
