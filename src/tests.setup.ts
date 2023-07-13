bccd71611aeb3942fde28563774764a1ff1c95de
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-13 08:20:46 +0200

    feat: add axe to vitest

--- src/tests.setup.ts
+++ src/tests.setup.ts
@@ -1,10 +1,13 @@
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
+import * as axeMatchers from 'vitest-axe/matchers';
 import { server } from './mocks/server'
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
+
+expect.extend(axeMatchers);
 
 beforeAll(() => {
   // Enable the mocking in tests.
