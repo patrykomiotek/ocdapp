5b34168948081fae86047a7d14b74eebe4e75544
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-07-13 10:15:25 +0200

    feat: test for auth credentials

--- src/components/Auth/AuthCredentials/AuthCredentials.spec.tsx
+++ src/components/Auth/AuthCredentials/AuthCredentials.spec.tsx
@@ -1,14 +1,15 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { AuthCredentials } from './AuthCredentials';
+import { AuthProvider } from '../AuthContext';
 
 describe('AuthCredentials', () => {
   it('should change display value', () => {
-    render(<AuthCredentials />);
+    render(<AuthProvider><AuthCredentials /></AuthProvider>);
 
     expect(screen.getByText(/is user logged\? no/i));
 
     fireEvent.click(screen.getByRole('button', { name: /change value/i }));
 
-    screen.debug();
+    expect(screen.getByText(/is user logged\? yes/i));
   })
 });
